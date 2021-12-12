4c7603865d0cca172572bfc0435099651f561dee
RainbowJM <Jevona98@live.com>
2021-12-13 00:50:28 +0100

    working on the commands

--- TableService/src/main/java/com/restaurant/TableService/core/application/TableCommandService.java
+++ TableService/src/main/java/com/restaurant/TableService/core/application/TableCommandService.java
@@ -1,4 +1,15 @@
 package com.restaurant.TableService.core.application;
 
+import com.restaurant.TableService.core.port.TableRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class TableCommandService {
+    private final TableRepository tableRepository;
+
+    public TableCommandService(TableRepository tableRepository) {
+        this.tableRepository = tableRepository;
+    }
 }
