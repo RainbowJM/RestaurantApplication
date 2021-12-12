be029677a838782ed57ff0b3fcb0c0b913b3dede
RainbowJM <Jevona98@live.com>
2021-12-12 22:19:00 +0100

    added eventlistener

---
+++ TableService/src/main/java/com/restaurant/TableService/adapter/message/TableEventListener.java
@@ -0,0 +1,13 @@
+package com.restaurant.TableService.adapter.message;
+
+import com.restaurant.TableService.core.application.TableCommandService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TableEventListener {
+    private TableCommandService  tableCommandService;
+
+    public TableEventListener(TableCommandService tableCommandService) {
+        this.tableCommandService = tableCommandService;
+    }
+}
---
+++ TableService/src/main/java/com/restaurant/TableService/core/application/TableCommandService.java
@@ -0,0 +1,4 @@
+package com.restaurant.TableService.core.application;
+
+public class TableCommandService {
+}
