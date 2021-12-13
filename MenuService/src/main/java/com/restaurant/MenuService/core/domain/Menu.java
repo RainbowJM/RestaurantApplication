6c24b811db0a6c33a49b035cab1e8088f9e94751
Jp <jan-paul.vandermoolen@student.hu.nl>
2021-12-13 12:13:14 +0100

    More changes, lombok is setup aswell, finishing touches on the domain and refactoring positioning of the folders

--- MenuService/src/main/java/com/restaurant/MenuService/core/domain/Menu.java
+++ MenuService/src/main/java/com/restaurant/MenuService/core/domain/Menu.java
@@ -1,18 +1,25 @@
 package com.restaurant.MenuService.core.domain;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.annotation.processing.Generated;
-import java.lang.reflect.Array;
 import java.util.List;
 
 @Document(collation = "Menu")
 public class Menu {
 
     @Id
+    @Getter
     private long id;
+
+    @Getter
+    @Setter
     private List dishes;
+
+    @Id
+    @Getter
     private long restaurantId;
 
     public Menu() {}
--- MenuService/src/main/java/com/restaurant/MenuService/data/MenuDTO.java
+++
@@ -1,9 +0,0 @@
-package com.restaurant.MenuService.data;
-
-import java.lang.reflect.Array;
-
-public class MenuDTO {
-	private long id;
-	private Array dishes;
-	private long restaurantId;
-}
