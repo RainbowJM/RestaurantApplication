54aa9fedbde70cf8cb0c50b57261b05e172c39a0
RainbowJM <Jevona98@live.com>
2021-12-17 01:17:40 +0100

    added modify table controller, service, command

--- TableService/src/main/java/com/restaurant/TableService/core/application/TableCommandService.java
+++ TableService/src/main/java/com/restaurant/TableService/core/application/TableCommandService.java
@@ -2,11 +2,15 @@ package com.restaurant.TableService.core.application;
 
 import com.restaurant.TableService.core.application.command.AddTableCommand;
 import com.restaurant.TableService.core.application.command.DeleteTableCommand;
+import com.restaurant.TableService.core.application.command.ModifyTableCommand;
 import com.restaurant.TableService.core.domain.Table;
 import com.restaurant.TableService.core.domain.exception.FailedToDeleteTable;
+import com.restaurant.TableService.core.domain.exception.TableNotFound;
 import com.restaurant.TableService.core.port.TableRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -23,6 +27,17 @@ public class TableCommandService {
         return tableRepository.save(new Table(addTableCommand.restaurantId(), addTableCommand.location(), addTableCommand.numberOfSeats()));
     }
 
+    public Table handle(ModifyTableCommand modifyTableCommand){
+        Optional<Table> optTable = this.tableRepository.findById(modifyTableCommand.tableId());
+
+        if (optTable.isEmpty()){
+            throw new TableNotFound(modifyTableCommand.tableId());
+        }
+
+        Table table = optTable.get();
+        table.changeTable(modifyTableCommand);
+        return table;
+    }
     public void handle(DeleteTableCommand deleteTableCommand) {
         if (this.tableRepository.deleteTableById(deleteTableCommand.id()).isEmpty()) {
             throw new FailedToDeleteTable(deleteTableCommand.id());
