8c5f81bf822ec8b4009c92eae6c62d5f993bcbe6
Jp <jan-paul.vandermoolen@student.hu.nl>
2021-12-20 12:15:29 +0100

    Changed rest controller mapping names and change value names to english

--- MenuService/src/main/java/com/restaurant/MenuService/core/domain/Dish.java
+++ MenuService/src/main/java/com/restaurant/MenuService/core/domain/Dish.java
@@ -5,16 +5,16 @@ import java.util.List;
 public class Dish {
 	private String id;
 	private String naam;
-	private Double prijs;
-	private List<String> ingredienten;
-	private int calorie;
+	private Double price;
+	private List<String> ingredients;
+	private int calories;
 
 	public Dish(String id, String naam, Double prijs, List<String> ingredienten, int calorie){
 		this.id = id;
 		this.naam = naam;
-		this.prijs = prijs;
-		this.ingredienten = ingredienten;
-		this.calorie = calorie;
+		this.price = prijs;
+		this.ingredients = ingredienten;
+		this.calories = calorie;
 	}
 
 	public String getId() {
