b5f5f18b046438c772998a7b339ffb20bfa3f900
Jp <jan-paul.vandermoolen@student.hu.nl>
2021-12-23 22:21:33 +0100

    Created, changed en removed events for event publisher gecreated

--- MenuService/src/main/java/com/restaurant/MenuService/core/domain/event/MenuCreatedEvent.java
+++ MenuService/src/main/java/com/restaurant/MenuService/core/domain/event/MenuCreatedEvent.java
@@ -1,4 +1,23 @@
 package com.restaurant.MenuService.core.domain.event;
 
-public class MenuCreatedEvent {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@AllArgsConstructor
+public class MenuCreatedEvent extends MenuEvent{
+	public static final String KEY = "menu.event.created";
+
+	@Override
+	public String getEventKey() { return KEY; }
+
+	@Getter
+	private final String id;
+
+	@Getter
+	private final List<String> dishIds;
+
+	@Getter
+	private final String restaurantId;
 }
