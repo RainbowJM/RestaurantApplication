809aa98e1aa5d07c3713f015ba3abaf5f53ff707
RainbowJM <Jevona98@live.com>
2021-12-23 14:16:29 +0100

    added eventpublisher for delete table

--- TableService/src/main/java/com/restaurant/TableService/core/application/TableCommandService.java
+++ TableService/src/main/java/com/restaurant/TableService/core/application/TableCommandService.java
@@ -6,6 +6,7 @@ import com.restaurant.TableService.core.application.command.DeleteTableCommand;
 import com.restaurant.TableService.core.application.command.ModifyTableCommand;
 import com.restaurant.TableService.core.domain.Table;
 import com.restaurant.TableService.core.domain.event.TableAddedEvent;
+import com.restaurant.TableService.core.domain.event.TableRemovedEvent;
 import com.restaurant.TableService.core.domain.exception.FailedToDeleteTable;
 import com.restaurant.TableService.core.domain.exception.RestaurantNotFound;
 import com.restaurant.TableService.core.domain.exception.TableNotFound;
@@ -36,7 +37,7 @@ public class TableCommandService {
         }
 
         // todo: publish events when a table has been deleted
-        Table createdTable = tableRepository.save(new Table(addTableCommand.restaurantId(), addTableCommand.location(), addTableCommand.numberOfSeats()));
+        Table createdTable = this.tableRepository.save(new Table(addTableCommand.restaurantId(), addTableCommand.location(), addTableCommand.numberOfSeats()));
 
         if (createdTable != null) {
             this.eventPublisher.publish(
@@ -61,10 +62,13 @@ public class TableCommandService {
     }
 
     public void handle(DeleteTableCommand deleteTableCommand) {
-        if (this.tableRepository.deleteTableById(deleteTableCommand.id()).isEmpty()) {
+        if (this.tableRepository.deleteTableById(deleteTableCommand.id()) != null) {
             throw new FailedToDeleteTable(deleteTableCommand.id());
         }
 
-        this.tableRepository.deleteTableById(deleteTableCommand.id());
+        Table deletedTable = this.tableRepository.deleteTableById(deleteTableCommand.id());
+        if (deletedTable != null) {
+            this.eventPublisher.publish(new TableRemovedEvent(deleteTableCommand.id()));
+        }
     }
 }
