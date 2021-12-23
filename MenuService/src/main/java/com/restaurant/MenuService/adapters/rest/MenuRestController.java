ac07d7d158792fa770401f9a5b9f3edd24ac58ba
Jp <jan-paul.vandermoolen@student.hu.nl>
2021-12-23 13:22:19 +0100

    Menu rest controller with authentication

--- MenuService/src/main/java/com/restaurant/MenuService/adapters/rest/MenuRestController.java
+++ MenuService/src/main/java/com/restaurant/MenuService/adapters/rest/MenuRestController.java
@@ -12,9 +12,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import javax.management.InstanceAlreadyExistsException;
 import javax.management.InstanceNotFoundException;
 import javax.validation.Valid;
+import java.nio.file.AccessDeniedException;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -29,33 +33,39 @@ public class MenuRestController {
 	}
 
 	@GetMapping(path="/{menuId}/")
+	@RolesAllowed({"User", "Staff", "Management", "OtherService"})
 	@ResponseStatus(HttpStatus.OK)
 	public Optional<Menu> getMenuById(@PathVariable String menuId){return this.menuQueryService.getMenuById(menuId);
 	}
 
 	@GetMapping(path="/restaurant/{menuId}/")
+	@RolesAllowed({"User", "Staff", "Management", "OtherService"})
 	@ResponseStatus(HttpStatus.OK)
 	public Optional<Menu> getMenuByRestaurantId(@PathVariable String menuId){
 		return this.menuQueryService.getMenuByRestaurantId(menuId);
 	}
 
 	@PostMapping(path="/")
+	@RolesAllowed({"Staff", "Management"})
 	@ResponseStatus(HttpStatus.CREATED)
 	public Menu createMenu(@Valid @RequestBody MenuRequest menuRequest) throws InstanceAlreadyExistsException {
 		return this.menuCommandService.handle(new AddMenuCommand(menuRequest.id, menuRequest.restaurantId));
 	}
 
 	@DeleteMapping(path = "/{menuId}/")
+	@RolesAllowed({"Staff", "Management"})
 	@ResponseStatus(HttpStatus.OK)
 	public void deleteMenu(@PathVariable String menuId) throws InstanceNotFoundException {this.menuCommandService.handle(new DeleteMenuCommand(menuId));}
 
 	@GetMapping(path = "/{menuId}/{dishId}")
+	@RolesAllowed({"User", "Staff", "Management", "OtherService"})
 	@ResponseStatus(HttpStatus.OK)
 	public Dish getDishfromMenu(@PathVariable String menuId, @PathVariable String dishId){
 		return this.menuQueryService.getDishByMenu(menuId, dishId);
 	}
 
 	@PostMapping("/{menuId}/")
+	@RolesAllowed({"Staff", "Management"})
 	@ResponseStatus(HttpStatus.CREATED)
 	public DishRequest addDishToMenu(@RequestBody DishRequest dishRequest, @PathVariable String menuId){
 		this.menuQueryService.getMenuById(menuId).get().getDishes().add(new Dish(dishRequest.id, dishRequest.naam, dishRequest.prijs, dishRequest.ingredienten, dishRequest.calories));
@@ -63,8 +73,14 @@ public class MenuRestController {
 	}
 
 	@DeleteMapping("/{menuId}/{dishId}/")
+	@RolesAllowed({"Staff", "Management", "OtherService"})
 	@ResponseStatus(HttpStatus.OK)
 	public void deleteDishFromMenu(@PathVariable String menuId, @PathVariable String dishId){
 		this.menuQueryService.getMenuById(menuId).get().deleteDishById(dishId);
+	}
+
+	@ExceptionHandler({AccessDeniedException.class})
+	public ResponseEntity<Map<String, String>> handleOldTokenException(Exception exception) {
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(Collections.singletonMap("error", "Access was denied! You probably don't have the necessary privileges to call this endpoint or your login token has expired."));
 	}
 }
